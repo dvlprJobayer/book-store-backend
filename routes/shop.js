33e0e9cec73634dfa45c0268a17466c1fcc5512e
dvlprjobayer <nesarahammed019@gmail.com>
2023-06-04 22:09:52 +0600

    shop route added

---
+++ routes/shop.js
@@ -0,0 +1,30 @@
+const router = require("express").Router();
+const { body } = require("express-validator");
+
+const shopController = require("../controllers/shop");
+
+router.get("/books", shopController.getAllBooks);
+
+router.get("/book/:id", shopController.getSingleBook);
+
+router.put("/book/:id", shopController.addToCart);
+
+router.delete("/book/:id", shopController.removeFromCart);
+
+router.post(
+  "/cart",
+  body("cart", "cart has to be a non-empty array").isArray(),
+  shopController.postCart
+);
+
+router.get("/cart", shopController.getCart);
+
+router.get("/order", shopController.order);
+
+router.post("/order", shopController.addOrder);
+
+router.post("/pay/:id", shopController.pay);
+
+router.put("/pay/:id", shopController.confirmPay);
+
+module.exports = router;
